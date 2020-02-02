402e298a0faef3e0ea453a12da8091d97eb6ac95
Shayan Amani <7shayan7@gmail.com>
2020-02-01 19:54:29 -0500

    made the whole project work

--- video_source/ID/secret_id.py
+++ video_source/ID/secret_id.py
@@ -4,10 +4,13 @@
 import os
 
 
-
 def secret_client_file():
-	for file in os.listdir('./ID'):
-		if file.endswith(".json"):
-			selected_id = os.path.join("ID",file)
-			print('client_secret_id : ',selected_id)
-			return selected_id
+    for file in os.listdir('.'):
+        if file.endswith(".json"):
+            selected_id = os.path.join("ID", file)
+            print('client_secret_id : ', selected_id)
+            return selected_id
+
+
+if __name__ == '__main__':
+    secret_client_file()
